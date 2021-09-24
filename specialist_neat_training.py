0481cded08e7e039efa532b22f4dac20f64781bf
ThBellucci <40698255+thomas097@users.noreply.github.com>
2021-09-24 09:32:03 +0200

    Changed winner to best :)

--- specialist_neat_training.py
+++ specialist_neat_training.py
@@ -158,12 +158,12 @@ if __name__ == '__main__':
             
             # Set up population and run EA for several generations.
             pop = neat.Population(config)
-            winner = pop.run(env.evaluate_population, GENERATIONS)
+            best_genome = pop.run(env.evaluate_population, GENERATIONS)
 
             # Store winner genome using pickle (for later use).
-            winner_file = file_name.replace("stats", "winner").format(run, enemy, str(INDIVIDUAL_TYPE))
-            with open(winner_file + ".pkl", "wb") as f:
-                pickle.dump(winner, f)
+            best_file = file_name.replace("stats", "best").format(run, enemy, str(INDIVIDUAL_TYPE))
+            with open(best_file + ".pkl", "wb") as f:
+                pickle.dump(best_genome, f)
 
     
 
